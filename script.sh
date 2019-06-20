6347033f06643aea090bf1df4e4aec1a00622565
Ales Puchilo <ales.puchilo@gmail.com>
2019-06-20 16:51:28 +0300

    Fix readme bug

--- script.sh
+++ script.sh
@@ -28,3 +28,11 @@ project: js-amino
 cat CHANGELOG.md >> temp.md
 cat temp.md > CHANGELOG.md
 rm -rf temp.md
+
+touch temp.md
+echo "---
+project: js-amino
+---" >> temp.md
+cat README.md >> temp.md
+cat temp.md > README.md
+rm -rf temp.md
